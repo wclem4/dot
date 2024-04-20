fa9198f8b8090db3c0aec97e9f3f833e1c321e40
Walker Clem <wclem@novetta.com>
2024-04-20 03:03:43 +0000

    Updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,109 +1,16 @@
 vim.g.mapleader = ','
 
--- fix tabbing
+-- Fix tabbing
 vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.shiftwidth = 2
 
-vim.o.number = true      -- show numbers
-vim.o.signcolumn = "yes" -- always show the sign column left of numbers
-vim.o.ignorecase = true  -- ignore case when searching
-vim.o.scrolloff = 1      -- number of lines kept above/below cursor
-vim.o.termguicolors = true
+vim.o.number = true      -- Show numbers
+vim.o.signcolumn = 'yes' -- Always show sign column
+vim.o.ignorecase = true  -- Ignore case when searching
+vim.o.scrolloff = 1      -- Number of lines kept above/below cursor
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-  -- Theme
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'gruvbox'
-    end,
-    opts = {},
-  },
-
-  -- Fancy status line
-  {
-    'nvim-lualine/lualine.nvim',
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
-
-  "nvim-treesitter/nvim-treesitter",
-
-  -- Search
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim', }
-  },
-  'benfowler/telescope-luasnip.nvim',
-
-  -- Key dictionary popup
-  'folke/which-key.nvim',
-
-  -- LSP
-  'neovim/nvim-lspconfig',
-  'williamboman/mason.nvim',
-  'williamboman/mason-lspconfig.nvim',
-
-  -- Autocomplete
-  'hrsh7th/nvim-cmp',
-  'hrsh7th/cmp-nvim-lsp',
-  'hrsh7th/cmp-buffer',
-  'hrsh7th/cmp-path',
-  'saadparwaiz1/cmp_luasnip',
-
-  -- Snippets
-  {
-    'L3MON4D3/LuaSnip',
-    dependencies = { 'rafamadriz/friendly-snippets', }
-  },
-  {
-    'stevearc/conform.nvim',
-    opts = {},
-  },
-})
-
-require("conform").setup({
-  formatters_by_ft = {
-    javascript = { "prettier" },
-    typescript = { "prettier" }
-  },
-  format_on_save = {
-    async = true,
-  },
-})
-
-require("nvim-treesitter.configs").setup {
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "tsx", "sql", "go" },
-  highlight = {
-    enable = true
-  }
-}
-
-require("which-key").setup()
-
--- reopen file at last position
+-- Reopen file at last position
 vim.api.nvim_create_autocmd('BufReadPost', {
   pattern = '*',
   callback = function()
@@ -113,73 +20,202 @@ vim.api.nvim_create_autocmd('BufReadPost', {
   end
 })
 
-
--- vim.api.nvim_create_autocmd('BufWritePost', {
---   callback = function()
---     vim.lsp.buf.format()
---   end
--- })
-
--- copy paste between windows
+-- Copy/Paste between windows
 vim.keymap.set('v', '<leader>y', ':w! /tmp/vitmp<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>p', ':r! cat /tmp/vitmp<CR>', { noremap = true, silent = true })
 
--- telescope
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+
+  -- Color Scheme
+  {
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "kanagawa"
+    end,
+  },
+
+  -- Status Line
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+      'f-person/git-blame.nvim',
+    },
+  },
+
+  -- Formatter
+  'stevearc/conform.nvim',
+
+  -- Parser
+  "nvim-treesitter/nvim-treesitter",
+
+  -- Notifications
+  "j-hui/fidget.nvim",
+
+  -- Search
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.6',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    }
+  },
+
+  -- Key Dictionary
+  'folke/which-key.nvim',
+
+  -- Package Manager
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+
+  -- LSP
+  'neovim/nvim-lspconfig',
+
+  -- Completion
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-nvim-lsp',
+  'dcampos/nvim-snippy',
+  'dcampos/cmp-snippy'
+})
+
+-- Status Line Setup
+local git_blame = require('gitblame');
+vim.g.gitblame_display_virtual_text = 0;
+require('lualine').setup {
+  options = {
+    component_separators = '|',
+    section_separators = '',
+  },
+  sections = {
+    lualine_c = { { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available } }
+  }
+}
+
+-- Formatter Setup
+require("conform").setup({
+  formatters_by_ft = {
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    go = { "goimports", "gofmt" }
+  },
+  format_on_save = {
+    lsp_fallback = true,
+  },
+})
+
+-- Parser Setup
+require("nvim-treesitter.configs").setup {
+  ensure_installed = {
+    "javascript",
+    "typescript",
+    "tsx",
+    "sql",
+    "go",
+    "gomod",
+    "gosum",
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "c",
+  },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false
+  },
+}
+
+-- Notifications Setup
+require("fidget").setup()
+
+-- Search Setup
 require('telescope').setup()
-
 local tsb = require('telescope.builtin')
-
--- search files
 vim.keymap.set('n', '<leader>gf', tsb.git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>sf', tsb.find_files, { desc = '[S]earch [F]iles' })
-
--- search text
 vim.keymap.set('n', '<leader>sg', tsb.live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sw', tsb.grep_string, { desc = '[S]earch current [W]ord' })
-
--- search diagnostics
 vim.keymap.set('n', '<leader>sd', tsb.diagnostics, { desc = '[S]earch [D]iagnostics' })
-
--- resume previous search
 vim.keymap.set('n', '<leader>sr', tsb.resume, { desc = '[S]earch [R]esume' })
-
--- search vim help documentation
 vim.keymap.set('n', '<leader>sh', tsb.help_tags, { desc = '[S]earch [H]elp' })
-
--- search telescope built in functions
 vim.keymap.set('n', '<leader>ss', tsb.builtin, { desc = '[S]earch [S]elect Telescope' })
 
--- add snippets to telescope ('Telescope luasnip')
-require('telescope').load_extension('luasnip')
+-- Key Dictionary Setup
+require('which-key').setup()
 
--- LSP jumps/refactors
+-- Package Manager Setup
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "bashls",
+    "jsonls",
+    "gopls",
+    "sqlls",
+    "tsserver",
+    "lua_ls",
+    "eslint",
+  },
+  automatic_installation = true,
+})
+
+-- LSP Setup
+local lspconfig = require('lspconfig')
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+lspconfig.bashls.setup({ capabilities = lsp_capabilities })
+lspconfig.jsonls.setup({ capabilities = lsp_capabilities })
+lspconfig.gopls.setup({ capabilities = lsp_capabilities })
+lspconfig.sqlls.setup({ capabilities = lsp_capabilities })
+lspconfig.tsserver.setup({
+  capabilities = lsp_capabilities,
+  init_options = {
+    preferences = { includeCompletionsForModuleExports = false }
+  }
+})
+lspconfig.lua_ls.setup({
+  capabilities = lsp_capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' },
+      },
+    }
+  },
+})
+lspconfig.eslint.setup({ capabilities = lsp_capabilities })
+
 vim.api.nvim_create_autocmd('LspAttach', {
   desc = 'LSP actions',
   callback = function()
-    local bufmap = function(mode, lhs, rhs)
-      local opts = { buffer = true }
-      vim.keymap.set(mode, lhs, rhs, opts)
-    end
-
-    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
-    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
-    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
-    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
-    bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
-
-    bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
-    bufmap('n', '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>')
-    bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
-
-    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-    bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-    bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+    local opts = { buffer = true }
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
   end
 })
 
--- Diag
 local sign = function(opts)
   vim.fn.sign_define(opts.name, {
     texthl = opts.name,
@@ -197,94 +233,32 @@ vim.diagnostic.config({
   virtual_text = true,
   severity_sort = true,
   float = {
-    border = 'rounded',
     source = 'always',
   },
 })
 
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
-  vim.lsp.handlers.hover,
-  { border = 'rounded' }
-)
-
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-  vim.lsp.handlers.signature_help,
-  { border = 'rounded' }
-)
-
--- Mason Setup
-require("mason").setup()
-
-require("mason-lspconfig").setup({
-  ensure_installed = {
-    "bashls",
-    "jsonls",
-    "gopls",
-    "sqlls",
-    "tsserver",
-    "lua_ls",
-    "eslint",
-  },
-  automatic_installation = true,
-})
-
--- LSP config
-local lspconfig = require('lspconfig')
-local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-lspconfig.bashls.setup({
-  capabilities = lsp_capabilities,
-})
-lspconfig.jsonls.setup({
-  capabilities = lsp_capabilities,
-})
-lspconfig.gopls.setup({
-  capabilities = lsp_capabilities,
-})
-lspconfig.sqlls.setup({
-  capabilities = lsp_capabilities,
-})
-lspconfig.tsserver.setup({
-  capabilities = lsp_capabilities,
-})
-lspconfig.lua_ls.setup({
-  capabilities = lsp_capabilities,
-})
-lspconfig.eslint.setup({
-  capabilities = lsp_capabilities,
-})
-
--- Autocomplete
+-- Completion Setup
 vim.opt.completeopt = { 'menu', 'menuone' }
-require('luasnip.loaders.from_vscode').lazy_load()
-
 local cmp = require('cmp')
-local luasnip = require('luasnip')
 local select_opts = { behavior = cmp.SelectBehavior.Select }
-
 cmp.setup({
   snippet = {
     expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end
+      require('snippy').expand_snippet(args.body)
+    end,
   },
   sources = {
     { name = 'path' },
-    { name = 'luasnip' },
-    { name = 'nvim_lsp' },
     { name = 'buffer',  keyword_length = 2 },
-  },
-  window = {
-    documentation = cmp.config.window.bordered()
+    { name = 'nvim_lsp' },
   },
   formatting = {
     fields = { 'menu', 'abbr', 'kind' },
     format = function(entry, item)
       local menu_icon = {
-        nvim_lsp = 'lsp',
-        luasnip = 'snip',
-        buffer = 'buf',
-        path = 'path',
+        nvim_lsp = 'LSP',
+        buffer = 'Buffer',
+        path = 'Path',
       }
       item.menu = menu_icon[entry.source.name]
       return item
@@ -301,23 +275,7 @@ cmp.setup({
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
 
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = false }),
-
-    ['<C-f>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(1) then
-        luasnip.jump(1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-
-    ['<C-b>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
 
     ['<Tab>'] = cmp.mapping(function(fallback)
       local col = vim.fn.col('.') - 1
